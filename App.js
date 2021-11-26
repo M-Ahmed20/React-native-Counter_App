f5d965693c4e74ec9b228a50ee817a21cbc3276b
M-Ahmed20 <78465606+M-Ahmed20@users.noreply.github.com>
2021-11-26 09:06:42 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+// import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Route from './config.js/routes';
+
+
+export default function App() {
+  return (
+    <Route/>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
